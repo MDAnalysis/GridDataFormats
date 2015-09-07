de58a7cc7ebcc99ef01d0b60d5b13f278a40105e
Max Linke <max_linke@gmx.de>
2015-09-07 10:32:56 +0200

    Add even more tests
    check compatibility for different grids and test all division
    functions in py2 and py3.

--- gridData/tests/test_grid.py
+++ gridData/tests/test_grid.py
@@ -1,6 +1,6 @@
 import numpy as np
 from numpy.testing import assert_array_equal
-from nose.tools import assert_equal
+from nose.tools import assert_equal, raises
 
 from gridData import Grid
 
@@ -22,22 +22,61 @@ class TestGrid:
         self.delta = np.ones(3)
         self.grid = Grid(self.griddata, origin=self.origin, delta=self.delta)
 
+    def test_equality(self):
+        assert self.grid == self.grid
+        assert self.grid != 'foo'
+        g = Grid(self.griddata, origin=self.origin + 1, delta=self.delta)
+        assert self.grid != g
+
     def test_addition(self):
         g = self.grid + self.grid
         assert_array_equal(g.grid.flat, (2 * self.griddata).flat)
+        g = 2 + self.grid
+        assert_array_equal(g.grid.flat, (2 + self.griddata).flat)
+        g = g + self.grid
+        assert_array_equal(g.grid.flat, (2 + (2 * self.griddata)).flat)
 
     def test_substraction(self):
         g = self.grid - self.grid
         assert_array_equal(g.grid.flat, np.zeros(27))
+        g = 2 - self.grid
+        assert_array_equal(g.grid.flat, (2 - self.griddata).flat)
 
     def test_multiplication(self):
         g = self.grid * self.grid
         assert_array_equal(g.grid.flat, (self.griddata ** 2).flat)
+        g = 2 * self.grid
+        assert_array_equal(g.grid.flat, (2 * self.griddata).flat)
 
     def test_division(self):
-        g = self.grid / self.grid
+        """ __truediv__ is used in py3 by default and py2 if division
+        is imported from __future__ to make testing easier lets call
+        them explicitely"""
+        g = self.grid.__truediv__(self.grid)
         assert_array_equal(g.grid.flat, np.ones(27))
+        g = self.grid.__rtruediv__(2)
+        assert_array_equal(g.grid.flat, (2 / self.griddata).flat)
+
+    def test_old_division(self):
+        """this is normally ONLY invoked in python 2. To have test
+        coverage in python3 as well call it explicitely"""
+        g = self.grid.__div__(self.grid)
+        assert_array_equal(g.grid.flat, np.ones(27))
+        g = self.grid.__rdiv__(2)
+        assert_array_equal(g.grid.flat, (2 / self.griddata).flat)
 
     def test_power(self):
         g = self.grid ** 2
         assert_array_equal(g.grid.flat, (self.griddata ** 2).flat)
+        g = 2 ** self.grid
+        assert_array_equal(g.grid.flat, (2 ** self.griddata).flat)
+
+    def test_compatibility_type(self):
+        assert self.grid.check_compatible(self.grid)
+        assert self.grid.check_compatible(3)
+        g = Grid(self.griddata, origin=self.origin - 1, delta=self.delta)
+        assert self.grid.check_compatible(g)
+
+    @raises(TypeError)
+    def test_wrong_compatibile_type(self):
+        self.grid.check_compatible("foo")
