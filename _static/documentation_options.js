16c954444a48d4e3e58293b7cbaf455b2ca94f8e
TravisCI <TravisCI@mdanalysis.org>
2018-08-15 23:51:59 +0000

    rebuilt html docs from branch master with sphinx at 0874b18

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.4.0+15.gf11cf51',
+    VERSION: '0.4.0+16.g0874b18',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
