6154342c0ab51fd15747c9df4db042d8552c10ff
Giacomo Fiorin <giacomo.fiorin@gmail.com>
2018-11-12 14:48:32 -0500

    Use (fixed-point) precision of NumPy array when writing DX file
    Again, this is a workaround for an issue in the PyMol DX parser that fails to
    parse tiny numbers (i.e. with an exponent lower than -100).  If it seems
    inconsistent with requiring "type double", it is because the DX parser uses a
    "float" to read the data:
    
    static int read_dx_data(void *v, int set, float *datablock,
                             float *colorblock) {
      ...
      float grid;
      ...
    
    Observed with PyMol up to 2.3.0.

--- gridData/OpenDX.py
+++ gridData/OpenDX.py
@@ -376,12 +376,16 @@ class array(DXclass):
         # grid data, serialized as a C array (z fastest varying)
         # (flat iterator is equivalent to: for x: for y: for z: grid[x,y,z])
         # VMD's DX reader requires exactly 3 values per line
+        fmt_string = "{:d}"
+        if (self.array.dtype.kind == 'f' or self.array.dtype.kind == 'c'):
+            precision = numpy.finfo(self.array.dtype).precision
+            fmt_string = "{:."+"{:d}".format(precision)+"f}"
         values_per_line = 3
         values = self.array.flat
         while 1:
             try:
                 for i in range(values_per_line):
-                    file.write(str(next(values)) + "\t")
+                    file.write(fmt_string.format(next(values)) + "\t")
                 file.write('\n')
             except StopIteration:
                 file.write('\n')
