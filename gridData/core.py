eb5356ce61c97642244b146b17e5a351505ee927
Anthony Cruz <acruzpr@users.noreply.github.com>
2020-04-17 04:24:38 -0400

    Fix centers function
    Centers should be displaced by self.delta * 0.5

--- gridData/core.py
+++ gridData/core.py
@@ -577,7 +577,7 @@ class Grid(object):
         """Returns the coordinates of the centers of all grid cells as an
         iterator."""
         for idx in numpy.ndindex(self.grid.shape):
-            yield self.delta * numpy.array(idx) + self.origin
+            yield self.delta * numpy.array(idx) + self.origin + (self.delta * 0.5)
 
     def check_compatible(self, other):
         """Check if *other* can be used in an arithmetic operation.
