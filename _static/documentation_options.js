29e7ece70b3e487741b8dc7332fbf69b3cc65414
IAlibay <IAlibay@users.noreply.github.com>
2021-09-14 23:59:08 +0000

    deploy: a1c29a660d29d9bae88a6746c80d1a7e7f7955f7

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.5.0+14.gbe6132a',
+    VERSION: '0+untagged.1.ga1c29a6',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
