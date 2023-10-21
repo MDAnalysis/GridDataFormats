8f069722ae51d207f5bc3bb7bf8dd9cc0e4ed815
Irfan Alibay <IAlibay@users.noreply.github.com>
2023-10-21 21:40:04 +0300

    Update for py312 (#128)
    * test on Python 3.12: Update gh-ci.yaml
    * bump up versioneer
    * update classifiers
    * fix rtd build

--- gridData/__init__.py
+++ gridData/__init__.py
@@ -113,7 +113,6 @@ from . import mrc
 
 __all__ = ['Grid', 'OpenDX', 'gOpenMol', 'mrc']
 
-from ._version import get_versions
-__version__ = get_versions()['version']
-del get_versions
+from . import _version
+__version__ = _version.get_versions()['version']
 
